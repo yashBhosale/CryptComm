fd680f2fa6f2207be6a83da5321bb9cfa046c7b2
yashBhosale <bhosaley5@gmail.com>
2017-06-10 02:42:20 -0400

    Create server.py

--- server.py
+++ server.py
@@ -1,45 +1,98 @@
+#completely untested as of 2:11 AM 6/11/17 
+
 import socket
 import sys
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
-from Crypto import Random
+import select
+import sqlite3
+import Queues
 
-
-key = b'very large generated key'
-
-iv = Random.new().read(AES.block_size)
-
-newCipher = AES.new(key,AES.MODE_CFB, iv)
-
-chatLog = open('chatlog.txt', 'w')
-
-chatLog.close()
+# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dataBase = sqlite3.connect('users.db')
+db = dataBase.cursor()
 
+# Bind socket to self
+server_address = (sock.gethostname(), 80)
 
+print >>sys.stderr, 'starting up on %s port %s' % server_address 
 
-server_address = ('192.168.0.2', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
-sock.listen(1)
+sock.listen(5)
 
-while True:
-	print >>sys.stderr, 'waiting for a connection'
-	connection, client_address = sock.accept()
-	chatLog = open('chatlog.txt', 'a')
-	try:
-		print >>sys.stderr, 'client connected:', client_address
-		while True:
-			data = connection.recv(64)
+
+#we need to check for stuff like 
+
+###############################################################
+
+
+inputs = [ sock ]
+messageQueues = {}
+
+while inputs:
+readable, writable, error = select(inputs, outputs, inputs)
+
+	for s in readable:
+		if s is server #this means there's a new connection
+
+			connection, client_address = sock.accept()
+			#I think this is where initial authentication goes
+
+			connection.setblocking(0)
 			
-			print >>sys.stderr, 'received "%s"' % data
-			if data:
-				connection.sendall(data)
-				encryptedMessage = iv + newCipher.encrypt(data)
-				chatLog.write(encryptedMessage + '\n')
-			else:
-				break
-	finally:
-		connection.close()
+			if usercheck(db, connection) is True:
+				inputs.append(connection)
+				messageQueues[connection] = Queues.queue()
+		
 
-	chatLog.close()
+		else:
+			data = connection.recv(64)
+			if data:
+				if s not in outputs
+					outputs.append(s)
+				for clients in messageQueues
+					messageQueues[client].put(data)
+			else:
+				if s in outputs
+					outputs.remove(s)
+					inputs.remove(s)
+					s.close()
+
+	for w in writable:
+		try:
+			nextMsg = messageQueues[w].get_nowait()
+		except: Queue.Empty:
+			print >>sys.stderr 'output queue for', w.getpeername(), 'is empty'
+			outputs.remove(w)
+		else: 
+			print >>sys.stderr 'sending %s to %s' % (nextMsg, w.getpeername())
+			w.send(nextMsg)
+
+	for e in exceptional:
+		print >>sts.stderr, 'handling exceptional condition for', s.getpeername()
+		inputs.remove(e)
+
+		if e in outputs:
+			outputs.remove(e)
+		s.close()
+
+		del messageQueues[e]
+
+def usercheck(db, connection):
+
+	#SANITIZE THESE
+	connection.send('Please input a username')
+	user = connection.recv(64)
+
+	connection.send('Please input  password')
+	password = connection.recv(64)
+	sanitized = (password,)
+
+	#hashing stuff
+	db.execute("SELECT password FROM users WHERE username=?", sanitized)
+	check = db.fetchone()
+
+	if password == check:
+		return True
+	else:
+		return False
+
