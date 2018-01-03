1cc48bbe1ac21d757bd1386d0128f2103cd25e87
yashBhosale <bhosaley5@gmail.com>
2018-01-03 14:51:26 -0500

    Update client.py

--- client.py
+++ client.py
@@ -42,28 +42,3 @@ while 1:
 			sys.stdout.flush()
 
   
-
-closeConnection = ('seshclose')
-message = ''
-while message is not closeConnection:
-message = raw_input()
-
-if message == 'seshclose':
-	sock.shutdown(1)
-    break
-
-sock.sendall(message)
-
-amount_received = 0
-amount_expected = len(message)
-while amount_received < amount_expected:
-    data = sock.recv(16)
-    amount_received += len(data)
-
-    
-
-finally:
-    sock.close()
-
-
-
